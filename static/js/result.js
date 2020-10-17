6f4f9edf2b4970d73b8e607eca8c7f66bc36400b
y-yana <66319933+y-yana@users.noreply.github.com>
2020-10-18 05:50:37 +0900

    ラベルに％表示

--- static/js/result.js
+++ static/js/result.js
@@ -2,7 +2,7 @@ var ctx = document.getElementById('pojinegaChart').getContext('2d');
 var chart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ['positive', 'negative'],
+    labels: ['positive(' + p_per + '%)', 'negative(' + n_per + '%)'],
     datasets: [{
       backgroundColor: ['#ff5ac8', '#2269e1'],
       data: [p_per, n_per]
